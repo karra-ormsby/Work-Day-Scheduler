114dba3aec01b141134a583e77fc795a1dd94498
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-03-21 20:50:59 +1100

    added text to README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Work-Day-Scheduler
+
+##Description
+
+Given some boilerplate code a daily planner, I have added to the code to create a functioning planner. The planner displays the current date at the top of the page. It displays time block for an average work day (9am to 5pm) with the colour of the time block changing depending on whether the time has passed (the time block/(s) will be grey), is the present time (the time block will be red), or in the future (the time block/(s) will be green). You can click into each time block and enter in an event and then save the event by pressing the save button. The data is then saved to local storage so that you can refresh the page and your event will persist. You can delete an old event and write down a new one to replace it. Once you are done you can hit save and the old event is deleted from storage and only the new event remains. 
+
+##Installation
+
+No installation needed. The code is deployed as a stand alone page on Git Pages, (https://karra-ormsby.github.io/Work-Day-Scheduler/).
+
+##Usage
+
+This website is a daily planner. At the top of the page you will see the current date displayed. 
+
+![current date](./assets/images/Current%20Date.png)
+
+Beneath that you will see time blocks for each hour of the average work week. Seen below as being 9am - 5pm.
+
+![time blocks](./assets/images/Time%20Blocks.png)
+
+Each time block will be one of three colour. 
+
+![colours of time blocks](./assets/images/Colours%20of%20time%20blocks.png)
+
+The grey represents a time that has already passed, the red represents the current hour, and the green represents time that is still to come in that day. 
+
+The user can click into any of the time blocks and type in an event. They can then click the save button to the right of the time block
+
+![save button](./assets/images/Save%20Button.png)
+
+to save their event.Their event is saved into local storage. Upon clicking the save button a message will be displayed below the date letting the user know that their input has been saved into local storage. 
+
+As the user input is saved into local storage the user can feel free to reload the page and have the confidence that their inout will persist (as long as they continue to use the same web browser).
+
+##Credits
+
+Starter code provided by Monash Bootcamp (https://git.bootcampcontent.com/Monash-University/MONU-VIRT-FSF-PT-02-2023-U-LOLC/-/tree/main/Week-05/02-Challenge)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,101 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
+$(document).ready(function () { 
+  var saveBtn = $(".saveBtn");
+  var saveIcon = $(".fa-save");
+  var savedToStorageEl = $("#saved-to-storage");
+  var header = $("header");
+  var eventHour;
+  var eventId;
+  var calendarEvent = JSON.parse(localStorage.getItem("calendarEvent")) || [];
 
-//documnet.ready
+  displayDate();
+  setHourBlocks();
+  getEvent();
 
+  //displays the current date
+  function displayDate() {
+    var today = dayjs().format('dddd DD[th of] MMMM');
+    $("#currentDay").text(today);
+  }
 
-var saveBtn = $(".saveBtn");
-var saveIcon = $(".fa-save");
-var savedToStorageEl = $("#saved-to-storage");
-var header = $("header");
-var eventHour;
-var eventId;
-var calendarEvent = JSON.parse(localStorage.getItem("calendarEvent")) || [];
+  //changes the colour of each hour block depending on whether it is past, present, or future
+  function setHourBlocks () {
+    //gets the current hour in 24 hour time and converts it into an int
+    var currentHour = parseInt(dayjs().format('HH'));
+    var hourBlockEl;
 
-displayDate();
-setHourBlocks();
+    for(i = 0; i <= 8; i++) {
+      //gets the <div> for each hour block
+      hourBlockEl = $('body').children("div").children().eq(i);
+      //gets the id of the <div> after the '-' and converts it into an int
+      hourNum = parseInt(hourBlockEl.attr("id").split('-')[1]);
 
-//displays the current date
-function displayDate() {
-  var today = dayjs().format('dddd DD[th of] MMMM');
-  $("#currentDay").text(today);
-}
-
-//changes the colour of each hour block depending on whether it is past, present, or future
-function setHourBlocks () {
-  //gets the current hour in 24 hour time and converts it into an int
-  var currentHour = parseInt(dayjs().format('HH'));
-  var hourBlockEl;
-
-  for(i = 0; i <= 8; i++) {
-    //gets the <div> for each hour block
-    hourBlockEl = $('body').children("div").children().eq(i);
-    //gets the id of the <div> after the '-' and converts it into an int
-    hourNum = parseInt(hourBlockEl.attr("id").split('-')[1]);
-
-    //compares the current hour with the hour of the hour block and changes the colour of each hour block depending on whether it is past, present, or future
-    if (currentHour < hourNum) {
-      hourBlockEl.addClass("future");
-    } else if (currentHour > hourNum) {
-      hourBlockEl.addClass("past");
-    } else {
-      hourBlockEl.addClass("present");
+      //compares the current hour with the hour of the hour block and changes the colour of each hour block depending on whether it is past, present, or future
+      if (currentHour < hourNum) {
+        hourBlockEl.addClass("future");
+      } else if (currentHour > hourNum) {
+        hourBlockEl.addClass("past");
+      } else {
+        hourBlockEl.addClass("present");
+      }
     }
   }
-}
 
-//when user clicks the blue save button then a function is run to save the event to local storage
-saveBtn.on("click", function(event) {
-  var element = event.target;
-  saveEvent(element)
-});
+  //when user clicks the blue save button then a function is run to save the event to local storage
+  saveBtn.on("click", function(event) {
+    var element = event.target;
+    saveEvent(element)
+  });
 
-//if the user clicks the floppy disc icon instead of the blue button it will still run the function to save the event to local storage
-saveIcon.on("click", function(event){
-  event.stopPropagation();
-  var element = event.target;
-  var button = $(element).parent();
-  saveEvent(button);
-});
+  //if the user clicks the floppy disc icon instead of the blue button it will still run the function to save the event to local storage
+  saveIcon.on("click", function(event){
+    event.stopPropagation();
+    var element = event.target;
+    var button = $(element).parent();
+    saveEvent(button);
+  });
 
-//gets the event inputed from the user and saves it to local storage
-function saveEvent (element) {
-  //gets the <textarea> of the save button that has been clicked
-  var textArea = $(element).prev();
-  //gets the value from the <textarea>
-  var textInput = textArea.val();
+  //gets the event inputed from the user and saves it to local storage
+  function saveEvent (element) {
+    //gets the <textarea> of the save button that has been clicked
+    var textArea = $(element).prev();
+    //gets the value from the <textarea>
+    var textInput = textArea.val();
 
-  //gets the <div> hour block assoicated with the save button and <textarea>
-  eventHour = textArea.parent();
-  //gets the id from the <div>
-  eventId = eventHour.attr("id");
+    //gets the <div> hour block assoicated with the save button and <textarea>
+    eventHour = textArea.parent();
+    //gets the id from the <div>
+    eventId = eventHour.attr("id");
 
-  //if there is already an input for a timeslot, delete that event and replace it with the new one
-  for(i = 0; i < calendarEvent.length; i++) {
-    if (calendarEvent[i].hour === eventId) {
-      delete calendarEvent[i].hour;
-      delete calendarEvent[i].input;
-      calendarEvent.splice(i, i);
+    //if there is already an input for a timeslot, delete that event and replace it with the new one
+    for(i = 0; i < calendarEvent.length; i++) {
+      if (calendarEvent[i].hour === eventId) {
+        delete calendarEvent[i].hour;
+        delete calendarEvent[i].input;
+        calendarEvent.splice(i, i);
+      }
+    }
+    
+    //saved the user input into an object with the associated id of the hour block
+    var scheduledEvent = {
+      hour: eventId,
+      input: textInput
+    }
+
+    //saves the object to an array (either an empty array or an array from local storage)
+    calendarEvent.push(scheduledEvent);
+    //saves to local storage
+    localStorage.setItem("calendarEvent", JSON.stringify(calendarEvent));
+
+    //tells the user that their data has been saved
+    savedToStorageEl.text(textInput + " saved to local Storage!");
+    header.append(savedToStorageEl);
+  }
+
+  //retrieves events from local storage and writes it to the hour block it was saved into
+  function getEvent () {
+    for(i = 0; i < calendarEvent.length; i++) {
+      var savedEvent = $('body').find("#" + calendarEvent[i].hour).children("textarea");
+      savedEvent.append(calendarEvent[i].input);
     }
   }
-  
-  //saved the user input into an object with the associated id of the hour block
-  var scheduledEvent = {
-    hour: eventId,
-    input: textInput
-  }
-
-  //saves the object to an array (either an empty array or an array from local storage)
-  calendarEvent.push(scheduledEvent);
-  //saves to local storage
-  localStorage.setItem("calendarEvent", JSON.stringify(calendarEvent));
-
-  //tells the user that their data has been saved
-  savedToStorageEl.text(textInput + " saved to local Storage!");
-  header.append(savedToStorageEl);
-}
-
-getEvent();
-
-//retrieves events from local storage and writes it to the hour block it was saved into
-function getEvent () {
-  for(i = 0; i < calendarEvent.length; i++) {
-    var savedEvent = $('body').find("#" + calendarEvent[i].hour).children("textarea");
-    savedEvent.append(calendarEvent[i].input);
-  }
-}
+});
